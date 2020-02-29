2765092d7765034d90ee6b4d442adb4ff9ade15a
Hiroki Koketsu <hiroki.koketsu@gmail.com>
2020-02-29 15:51:23 -0800

    Add entities

---
+++ WorldCities/Data/Models/City.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCities.Data.Models
+{
+	public class City
+	{
+		[Key]
+		[Required]
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+		public string Name_ASCII { get; set; }
+		public decimal Lat { get; set; }
+		public decimal Lon { get; set; }
+		[ForeignKey("Country")]
+		public int CountryId { get; set; }
+
+		public virtual Country Country { get; set; }
+	}
+}
