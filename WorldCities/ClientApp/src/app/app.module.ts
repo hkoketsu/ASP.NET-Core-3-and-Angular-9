76220a5de77ed5d0fe40c93a0f3baded61cca69e
Hiroki Koketsu <hiroki.koketsu@gmail.com>
2020-03-03 06:56:27 -0800

    Add editing feature

--- WorldCities/ClientApp/src/app/app.module.ts
+++ WorldCities/ClientApp/src/app/app.module.ts
@@ -5,20 +5,27 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BaseFormComponent } from './base.form.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CitiesComponent } from './cities/cities.component';
+import { CityEditComponent } from './cities/city-edit.component';
 import { CountriesComponent } from './countries/countries.component';
+import { CountryEditComponent } from './countries/country-edit.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BaseFormComponent,
     NavMenuComponent,
     HomeComponent,
     CitiesComponent,
-    CountriesComponent
+    CityEditComponent,
+    CountriesComponent,
+    CountryEditComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -27,10 +34,15 @@ import { AngularMaterialModule } from './angular-material.module';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'cities', component: CitiesComponent },
-      { path: 'countries', component: CountriesComponent }
+      { path: 'city/:id', component: CityEditComponent },
+      { path: 'city', component: CityEditComponent },
+      { path: 'countries', component: CountriesComponent },
+      { path: 'country/:id', component: CountryEditComponent },
+      { path: 'country', component: CountryEditComponent }
     ]),
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
